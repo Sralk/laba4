ad1f3be7f2f8d26973bfafa047327bd9bf588106
Sralk <panenko1900@mail.ru>
2019-12-27 01:24:33 +0300

    vd

--- cmake/HunterGate.cmake
+++ cmake/HunterGate.cmake
@@ -458,7 +458,7 @@ macro(HunterGate)
         HUNTER_GATE_ROOT "${HUNTER_GATE_ROOT}" ABSOLUTE
     )
     hunter_gate_status_debug("HUNTER_ROOT: ${HUNTER_GATE_ROOT}")
-    if(NOT HUNTER_ALLOW_SPACES_IN_PATH)
+    if(HUNTER_ALLOW_SPACES_IN_PATH)
       string(FIND "${HUNTER_GATE_ROOT}" " " _contain_spaces)
       if(NOT _contain_spaces EQUAL -1)
         hunter_gate_fatal_error(
